57dc2c2c0fca70f57cbb01acbded32cd72502403
Taras Teslyuk <taras.teslyuk@gmail.com>
2024-01-25 18:45:11 +0200

    add: basic bottom menu;

--- library/src/main/java/com/tetalab/logcollector/LogsActivity.kt
+++ library/src/main/java/com/tetalab/logcollector/LogsActivity.kt
@@ -3,12 +3,21 @@ package com.tetalab.logcollector
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tetalab.logcollector.ui.AboutFragment
+import com.tetalab.logcollector.ui.HistoryFragment
 import com.tetalab.logcollector.ui.LogFragment
+
 
 class LogsActivity : AppCompatActivity() {
 
     private lateinit var handler: Handler
+
+    private lateinit var bottomNavView: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +31,11 @@ class LogsActivity : AppCompatActivity() {
     }
 
     private fun initView() {
+        bottomNavView = findViewById(R.id.bottom_navigation)
     }
 
     private fun initListener() {
-
+        bottomNavView.setOnNavigationItemSelectedListener(navListener)
     }
 
     private fun showLogsFragment() {
@@ -34,4 +44,29 @@ class LogsActivity : AppCompatActivity() {
             .add(R.id.fragmentContainer, LogFragment())
             .commit()
     }
+
+    private val navListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            var selectedFragment: Fragment? = null
+            val itemId = item.itemId
+            if (itemId == R.id.session) {
+                selectedFragment = LogFragment()
+            } else if (itemId == R.id.history) {
+                selectedFragment = HistoryFragment()
+            } else if (itemId == R.id.about) {
+                selectedFragment = AboutFragment()
+            }
+            // It will help to replace the
+            // one fragment to other.
+            if (selectedFragment != null) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, selectedFragment)
+                    .commit()
+            }
+            true
+        }
 }
---
+++ library/src/main/java/com/tetalab/logcollector/ui/AboutFragment.kt
@@ -0,0 +1,25 @@
+package com.tetalab.logcollector.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tetalab.logcollector.R
+
+class AboutFragment: Fragment() {
+    private lateinit var root: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        root = inflater.inflate(R.layout.fragment_just_logs, container, false)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
